17802a86636d6ef7eef5ed10c59bf00867647a10
Bilal0313 <Bilalahmat222@gmail.com>
2023-12-04 18:29:31 -0500

    done

--- src/test/java/com/cydeo/tests/Day9_popups_tabs_frames/IframeTest.java
+++ src/test/java/com/cydeo/tests/Day9_popups_tabs_frames/IframeTest.java
@@ -1,9 +1,11 @@
 package com.cydeo.tests.Day9_popups_tabs_frames;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class IframeTest {
 
@@ -15,6 +17,34 @@ public class IframeTest {
         Thread.sleep(3000);
     }
 
+    @Test
+    public void test1() throws InterruptedException {
+        driver.get("http://practice.cydeo.com/iframe");
+        driver.switchTo().frame("mce_0_ifr");
+        Thread.sleep(3000);
+        driver.findElement(By.cssSelector("#tinymce")).clear();
+        driver.findElement(By.cssSelector("#tinymce")).sendKeys("Mike Smith");
+    }
+
+    @Test
+    public void test2(){
+        driver.get("https://practice.cydeo.com/nested_frames");
+        driver.switchTo().frame("frame-top");
+
+        driver.switchTo().frame("frame-middle");
+        System.out.println(driver.findElement(By.id("content")).getText());
+
+
+        driver.switchTo().parentFrame();
+        driver.switchTo().frame(2);
+        System.out.println(driver.findElement(By.tagName("body")).getText());
+
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame(1);
+        System.out.println(driver.findElement(By.tagName("body")).getText());
+
+
+    }
 
 
 
