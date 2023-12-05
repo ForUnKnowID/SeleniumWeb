c5e390381385a3aa94075b64760f6261ec4f759f
Bilal0313 <Bilalahmat222@gmail.com>
2023-12-05 10:29:16 -0500

    New class add

---
+++ src/test/java/com/cydeo/tests/Day10_actions_js/Actions_js.java
@@ -0,0 +1,31 @@
+package com.cydeo.tests.Day10_actions_js;
+
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Actions_js {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+    driver= WebDriverFactory.getDriver("chrome");
+    driver.manage().window().maximize();
+    }
+
+
+    @Test
+    public void test1(){
+
+    }
+
+
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+}
