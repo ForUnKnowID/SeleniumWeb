dddf3c8bf865bb9e6ff6348bdc303d7a990c5d9e
Bilal0313 <Bilalahmat222@gmail.com>
2023-12-06 16:34:53 -0500

    update

--- src/test/java/com/cydeo/tests/Day10_actions_js/JavaScriptDemo.java
+++ src/test/java/com/cydeo/tests/Day10_actions_js/JavaScriptDemo.java
@@ -1,7 +1,10 @@
 package com.cydeo.tests.Day10_actions_js;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -22,8 +25,15 @@ public class JavaScriptDemo {
 
     @Test
     public void clickWithJs(){
+        driver.get("https://practice.cydeo.com/");
+        WebElement dropDownLink = driver.findElement(By.linkText("Dropdown"));
+        JavascriptExecutor jse = (JavascriptExecutor) driver;
+        jse.executeScript("arguments[0].click;",dropDownLink);
+
+
 
     }
+
 
     @AfterMethod
     public void tearDown(){
