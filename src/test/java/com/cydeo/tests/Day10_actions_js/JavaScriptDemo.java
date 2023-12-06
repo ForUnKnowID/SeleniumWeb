7b1cb3f9f54fa5a61a6266e1c4b685b3251b7f97
Bilal0313 <Bilalahmat222@gmail.com>
2023-12-06 16:56:23 -0500

    update

--- src/test/java/com/cydeo/tests/Day10_actions_js/JavaScriptDemo.java
+++ src/test/java/com/cydeo/tests/Day10_actions_js/JavaScriptDemo.java
@@ -1,10 +1,8 @@
 package com.cydeo.tests.Day10_actions_js;
 
 import com.cydeo.utilities.WebDriverFactory;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -32,6 +30,17 @@ public class JavaScriptDemo {
 
 
 
+    }
+
+    @Test
+    public void typeWithJs(){
+        driver.get("https://practice.cydeo.com/dynamic_controls");
+
+        WebElement inputBox= driver.findElement(By.cssSelector("#input-example>input"));
+        JavascriptExecutor jsc = (JavascriptExecutor) driver;
+        String text = "Hello Disable Input";
+        jsc.executeScript("argument[0].setAttribute('value','"+text+"')",inputBox);
+        Actions j = new Actions(driver);
     }
 
 
