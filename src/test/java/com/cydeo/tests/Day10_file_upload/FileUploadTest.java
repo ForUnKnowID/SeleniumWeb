281b071886f06e3bac62b9a0d5953d156e3d7365
Bilal0313 <Bilalahmat222@gmail.com>
2023-12-15 02:16:37 -0500

    Updates For All And Done

--- src/test/java/com/cydeo/tests/Day10_file_upload/FileUploadTest.java
+++ src/test/java/com/cydeo/tests/Day10_file_upload/FileUploadTest.java
@@ -1,8 +1,13 @@
 package com.cydeo.tests.Day10_file_upload;
 
 import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import java.time.Duration;
 
@@ -15,7 +20,32 @@ public class FileUploadTest {
     driver = WebDriverFactory.getDriver("chrome");
     driver.manage().window().maximize();
     driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+
+    @Test
+    public void test1(){
+        driver.get("http://practice.cydeo.com/upload");
+
+        WebElement chooseFile = driver.findElement(By.name("file"));
+
+        chooseFile.sendKeys("/Users/bilal/Desktop/=7.png");
+
+        driver.findElement(By.id("file-submit")).click();
+
+        String actualFileName = driver.findElement(By.id("uploaded-files")).getText();
+
+        Assert.assertEquals(actualFileName,"=7.png","Verifying the File name's are match");
+
+
+
 
     }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+
 
 }
