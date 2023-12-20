b0077d5cc10f9dd5e43def7d9b114dea81e6bafd
Bilal0313 <Bilalahmat222@gmail.com>
2023-12-20 11:42:23 -0500

    Add the day11 class

--- src/test/java/com/cydeo/tests/day11_webtables/WebTableExample.java
+++ src/test/java/com/cydeo/tests/day11_webtables/WebTableExample.java
@@ -134,8 +134,6 @@ public class WebTableExample {
 
     @Test
     public void getCellInRelationToAnotherCellInSameRow(){
-
-
         String firstName = "Jason";
         String xpath = "//table[@id='table1']//td[.='"+firstName+"']/../td[3]";
         WebElement email  = driver.findElement(By.xpath(xpath));
