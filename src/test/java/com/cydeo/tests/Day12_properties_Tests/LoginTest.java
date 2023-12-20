36087d6009e9b3f07a7a77008dd7701a5fd6fdc2
Bilal0313 <Bilalahmat222@gmail.com>
2023-12-20 14:24:43 -0500

    Add new class

---
+++ src/test/java/com/cydeo/tests/Day12_properties_Tests/LoginTest.java
@@ -0,0 +1,54 @@
+package com.cydeo.tests.Day12_properties_Tests;
+
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import com.cydeo.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        driver = Driver.get();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+
+    @Test
+    public void OpenBrowserWithConf(){
+        WebDriver driver = WebDriverFactory.getDriver(ConfigurationReader.get("browser"));
+        driver.get(ConfigurationReader.get("url"));
+        String userName = ConfigurationReader.get("driver_username");
+
+
+        String userPassword = ConfigurationReader.get("driver_password");
+
+        driver.findElement(By.xpath("//input[@type='text']")).sendKeys(userName);
+        driver.findElement(By.xpath("//input[@type='password']")).sendKeys(userPassword+ Keys.ENTER);
+
+
+
+
+    }
+
+
+
+
+}
