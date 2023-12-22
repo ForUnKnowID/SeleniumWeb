aefa6df4bd8ea63786446180b03d0e8ce10b9999
Bilal0313 <Bilalahmat222@gmail.com>
2023-12-21 21:39:25 -0500

    Updates and create

--- src/test/java/com/cydeo/tests/Day13_pom/PositiveLoginTest.java
+++ src/test/java/com/cydeo/tests/Day13_pom/PositiveLoginTest.java
@@ -1,9 +1,47 @@
 package com.cydeo.tests.Day13_pom;
 
-public class PositiveLoginTest {
+import com.cydeo.pages.LoginPage;
+import com.cydeo.tests.Day12_properties_Tests.TestBase;
+import com.cydeo.utilities.ConfigurationReader;
+import com.cydeo.utilities.Driver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PositiveLoginTest extends TestBase {
 
 
+    @Test
+    public void loginAsDriver(){
 
+        LoginPage loginPage = new LoginPage();
+
+        String username = ConfigurationReader.getProperty("driver_username");
+        String password = ConfigurationReader.getProperty("driver_password");
+
+        loginPage.userNameInput.sendKeys(username);
+        loginPage.passwordInput.sendKeys(password);
+        loginPage.loginBtn.click();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(),"https://qa1.vytrack.com/");
+
+    }
+
+    @Test
+    public void loginAsStoreManager(){
+
+        LoginPage loginPage = new LoginPage();
+
+        String username = ConfigurationReader.getProperty("storemanager_username");
+        String password = ConfigurationReader.getProperty("storemanager_password");
+
+        loginPage.userNameInput.sendKeys(username);
+        loginPage.passwordInput.sendKeys(password);
+        loginPage.loginBtn.click();
+
+        Assert.assertEquals(Driver.getDriver().getCurrentUrl(),"https://qa1.vytrack.com/");
+
+
+    }
 
 
 }
