289e9d7fd1af35c29fe46de7fb4af956d0b02460
Bilal0313 <Bilalahmat222@gmail.com>
2024-01-01 12:39:51 -0500

    add new

---
+++ src/test/java/com/cydeo/pages/DashboardPage.java
@@ -0,0 +1,8 @@
+package com.cydeo.pages;
+
+import org.testng.annotations.Test;
+
+public class DashboardPage extends BasePage{
+
+
+}
--- src/test/java/com/cydeo/tests/Day13_pom/PageSubtitleTest.java
+++ src/test/java/com/cydeo/tests/Day13_pom/PageSubtitleTest.java
@@ -1,6 +1,10 @@
 package com.cydeo.tests.Day13_pom;
 
+import com.cydeo.pages.DashboardPage;
+import com.cydeo.pages.LoginPage;
 import com.cydeo.tests.Day12_properties_Tests.TestBase;
+import com.cydeo.utilities.ConfigurationReader;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class PageSubtitleTest extends TestBase {
@@ -9,10 +13,17 @@ public class PageSubtitleTest extends TestBase {
     @Test
     public void test1(){
 
+            LoginPage login = new LoginPage();
 
+            login.loginAsDriver();
+
+            String expectedSubTitle = "Quick Launchpad";
+
+            DashboardPage  dashboardPage = new DashboardPage();
+
+            String actualSubtitle = dashboardPage.getPageSubTitle();
+
+            Assert.assertEquals(actualSubtitle,expectedSubTitle,"verity Subtitle");
 
     }
-
-
-
 }
