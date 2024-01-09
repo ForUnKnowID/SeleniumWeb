793c324668355c776acec00ee2355c86b56da52b
Bilal0313 <Bilalahmat222@gmail.com>
2024-01-09 12:32:25 -0500

    Update and done

--- src/test/java/com/cydeo/tests/Day12_properties_Tests/TestBase.java
+++ src/test/java/com/cydeo/tests/Day12_properties_Tests/TestBase.java
@@ -1,5 +1,8 @@
 package com.cydeo.tests.Day12_properties_Tests;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.WebDriver;
@@ -7,6 +10,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
@@ -16,6 +20,48 @@ public class TestBase {
    protected WebDriver driver;
    protected Actions actions;
    protected WebDriverWait wait;
+    //This class is used for starting and building reports
+    protected ExtentReports reports;
+
+    // this class is used to create HTML report file
+   protected ExtentHtmlReporter htmlReporter;
+
+    //this will define a test, enables adding logs,authors,test steps
+    protected ExtentTest extentLogger;
+
+    @BeforeTest
+    public void setUpTest(){
+
+        // initialize the class
+        reports = new ExtentReports();
+
+        // create a report path
+        String projectPath = System.getProperty("user.dir");
+        String path = projectPath+"/test-output/report.html";
+
+        //initialize the html reporter with the report path
+        htmlReporter = new ExtentHtmlReporter(path);
+
+        //attach the html report to report object
+        reports.attachReporter(htmlReporter);
+
+
+        // title in report
+        htmlReporter.config().setReportName("Vytrack Smoke Test");
+
+        // Set environment information
+        reports.setSystemInfo("Environment","QA");
+        reports.setSystemInfo("OS",System.getProperty("os.name"));
+
+
+
+    }
+
+    @AfterMethod
+    public void tearDownTest(){
+        // this is when the report is actually created
+        reports.flush();
+    }
 
     @BeforeMethod
     public void setUp(){
